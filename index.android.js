a19d4825b62f7a588d2a8bd30dae72bc55db0b2a
RonakKhandelwal <ronak.khandelwal@learner.manipal.edu>
2016-04-05 10:00:53 +0530

    Added row view

--- index.android.js
+++ index.android.js
@@ -3,7 +3,7 @@
  * https://github.com/facebook/react-native
  */
 
-import React, {
+ import React, {
   AppRegistry,
   Component,
   Image,
@@ -18,7 +18,7 @@ import React, {
 
 
 var MOCKED_MOVIES_DATA = [
-  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+{title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
 ];
 
 class FirstProject extends Component {
@@ -26,48 +26,62 @@ class FirstProject extends Component {
     var movie = MOCKED_MOVIES_DATA[0];
 
     var styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  thumbnail: {
-    width: 53,
-    height: 81,
-  },
-});
+      container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+      },
+      thumbnail: {
+        width: 53,
+        height: 81,
+      },
+      rightContainer: {
+        flex: 1,
+      },
+      title: {
+        fontSize: 20,
+        marginBottom: 8,
+        textAlign: 'center',
+      },
+      year: {
+        textAlign: 'center',
+      },
+    });
 
     return (
       <View style={styles.container}>
-        <Text>{movie.title}</Text>
-        <Text>{movie.year}</Text>
-        <Image
-          source={{uri: movie.posters.thumbnail}}
-          style={styles.thumbnail}
-        />
+      <Image
+      source={{uri: movie.posters.thumbnail}}
+      style={styles.thumbnail}
+      />
+      <View style={styles.rightContainer}>
+      <Text style={styles.title}>{movie.title}</Text>
+      <Text style={styles.year}>{movie.year}</Text>
       </View>
-    );
+      </View>
+      );
+    }
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+      fontSize: 20,
+      textAlign: 'center',
+      margin: 10,
+    },
+    instructions: {
+      textAlign: 'center',
+      color: '#333333',
+      marginBottom: 5,
+    },
+  });
 
-AppRegistry.registerComponent('FirstProject', () => FirstProject);
+  AppRegistry.registerComponent('FirstProject', () => FirstProject);
