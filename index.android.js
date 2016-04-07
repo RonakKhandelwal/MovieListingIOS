c03c58f64bdb30d6178dd4823bc9d59ff2b34243
RonakKhandelwal <ronak.khandelwal@learner.manipal.edu>
2016-04-07 10:07:26 +0530

    Fetching data

--- index.android.js
+++ index.android.js
@@ -12,6 +12,8 @@
   View
 } from 'react-native';
 
+var REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
+
 
 
 
@@ -22,47 +24,65 @@ var MOCKED_MOVIES_DATA = [
 ];
 
 class FirstProject extends Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      movies: null,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        this.setState({
+          movies: responseData.movies,
+        });
+      })
+      .done();
+  }
+
+
   render() {
-    var movie = MOCKED_MOVIES_DATA[0];
+    if (!this.state.movies) {
+      return this.renderLoadingView();
+    }
 
-    var styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#F5FCFF',
-      },
-      thumbnail: {
-        width: 53,
-        height: 81,
-      },
-      rightContainer: {
-        flex: 1,
-      },
-      title: {
-        fontSize: 20,
-        marginBottom: 8,
-        textAlign: 'center',
-      },
-      year: {
-        textAlign: 'center',
-      },
-    });
+    var movie = this.state.movies[0];
+    return this.renderMovie(movie);
+  }
 
+  renderLoadingView() {
     return (
       <View style={styles.container}>
-      <Image
-      source={{uri: movie.posters.thumbnail}}
-      style={styles.thumbnail}
-      />
-      <View style={styles.rightContainer}>
-      <Text style={styles.title}>{movie.title}</Text>
-      <Text style={styles.year}>{movie.year}</Text>
+        <Text>
+          Loading movies...
+        </Text>
       </View>
+    );
+  }
+
+  renderMovie(movie) {
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{uri: movie.posters.thumbnail}}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
       </View>
-      );
-    }
+    );
+  }
+
+
   }
 
   const styles = StyleSheet.create({
